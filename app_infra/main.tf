36aeb3b660be441245d6b89c11a719328603e81f
mtahran <mutahran@gmail.com>
2023-08-07 14:18:50 +0000

    updated security group rules

--- app_infra/main.tf
+++ app_infra/main.tf
@@ -40,7 +40,7 @@ resource "aws_security_group_rule" "ingress_ssh" {
   from_port         = 22
   to_port           = 22
   protocol          = "tcp"
-  cidr_blocks       = ["0.0.0.0/0"]
+  cidr_blocks       = var.jenkins_server_ip
   security_group_id = aws_security_group.sg_app_server.id
 }
 
--- app_infra/variables.tf
+++ app_infra/variables.tf
@@ -1,15 +1,15 @@
 variable "jenkins_server_ip" {
-  description = "CIDR of the jenkins server"
+  description = "CIDR of the jenkins server, home and office"
   type        = list(string)
-  default = [ "172.31.35.188/32" ]
+  default = [ "172.31.35.188/32", "98.227.136.153/32","209.122.40.225/32" ]
 }
 variable "http_cidr_block" {
-  description = "CIDR of the jenkins server"
+  description = "CIDR of home and office"
   type        = list(string)
   default = [ "98.227.136.153/32","209.122.40.225/32" ]
 }
 variable "key_name" {
-  description = "ssh key of my laptop"
+  description = "ssh key of jenkins server"
   type        = string
   default = "ubuntu@jenkins"
 }
